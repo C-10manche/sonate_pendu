74afe40f64d67eadabf10f5eaaa7a1431f9f0a06
C-10manche <168531812+C-10manche@users.noreply.github.com>
2024-05-06 22:10:54 +0200

    python refactorise

--- server.py
+++ server.py
@@ -59,47 +59,63 @@ def get_random_word():
     random_word = list_of_words[random_place]    
     return random_word
 
-def continue_to_play_hangman():
-    global playername, lifepoint, word_to_guess, indice, playerguess, result
+def verify_game_status():
+    #Check si le jeu doit continuer     
+    if lifepoint <= 0 or "_" not in indice :      
+        return render_template("result.html", lifepoint = lifepoint, word_to_guess = word_to_guess)
+    else :                
+        if app.debug:
+            return render_template("play.html", result = result, lifepoint = lifepoint, indice = indice, word_to_guess = word_to_guess, good_guess = good_guess, wrong_guess = wrong_guess)
+        else:
+            return render_template("play.html", result = result, lifepoint = lifepoint, indice = indice, word_to_guess = " ", good_guess = good_guess, wrong_guess = wrong_guess)
+
+def verify_guess():
+    global playername, lifepoint, word_to_guess, playerguess, result
     playerguess = request.form["playerguess"]
-    new_indice = []
     
-    #Check si le joueur a deviné la lettre
-    if playerguess in good_guess or playerguess in wrong_guess:
+    #Check si le joueur a bien deviné la lettre
+    if playerguess in (good_guess or wrong_guess):
         #On a ajouté cette ligne pour éviter au joueur de perdre des points de vie si il refresh sa page
         result = "Vous avez déjà utilisé cette lettre !"           
     elif playerguess in remove_accent(word_to_guess):
-        result = "OUI !" 
-        #Met à jour l'indice        
-        for x in range(len(word_to_guess)):
-            if playerguess == remove_accent(word_to_guess[x]):
-                new_indice.append(word_to_guess[x])
-            else :
-                new_indice.append(indice[x])                    
-        indice = "".join(new_indice)
+        result = "OUI !"
+        update_indice()
         good_guess.append(playerguess)
     else :
         result = "NON !"
         lifepoint -= 1
         wrong_guess.append(playerguess)
+        
+
+def update_indice():
+    global indice    
+    new_indice = []
     
-    playerguess = "0"
-    #Check si le jeu doit continuer     
-    if lifepoint <= 0 or "_" not in indice :      
-        return render_template("result.html", lifepoint = lifepoint, word_to_guess = word_to_guess)
-    else :
-        return render_template("play.html", result = result, lifepoint = lifepoint, indice = indice, word_to_guess = word_to_guess, good_guess = good_guess, wrong_guess = wrong_guess)
+    #Met à jour l'indice        
+    for x in range(len(word_to_guess)):
+        if playerguess == remove_accent(word_to_guess[x]):
+            new_indice.append(word_to_guess[x])
+        else :
+            new_indice.append(indice[x])                    
+    indice = "".join(new_indice)
+    
 
 def reset_game():
-    #Réinitialise les variables du jeu à leur valeur initial
     global list_of_words, lifepoint, word_to_guess, indice, playerguess, result, wrong_guess
+    
+    #Réinitialise les variables du jeu à leur valeur initial
     lifepoint = 5
     word_to_guess = get_random_word()
     good_guess.clear()
     wrong_guess.clear()
     indice = "_" * len(word_to_guess)
     result = "Cliquer sur les lettres pour trouver le mot caché"
-    return render_template("play.html", result = result, lifepoint = lifepoint, indice = indice, word_to_guess = word_to_guess, wrong_guess = wrong_guess)
+
+def register():    
+    global playername, list_of_words
+    list_of_words = open_dictionary()  
+    playername = request.form["playername"] 
+    reset_game()       
 
 app = Flask(__name__)
 @app.route("/")
@@ -108,18 +124,18 @@ def home():
 
 @app.route("/play", methods=["POST"])
 def play():
-    global playername, list_of_words    
+    global playername, list_of_words
+       
     #Gère les actions du joueur
     if request.method == "POST":
         if "playername" in request.form:
-            list_of_words = open_dictionary()  
-            playername = request.form["playername"] 
-            return reset_game()       
+            register()    
         elif "playerguess" in request.form:
-            return continue_to_play_hangman()
+            verify_guess()
         elif "replay" in request.form:
-            return reset_game()
-    
+            reset_game()
+                       
+    return verify_game_status()
 
 #flask --app server run
 #flask --app server --debug run
